398fff2e9654c7b8338597e4f654087da0ac00a9
rebecca mulugeta <becc.mulugeta@gmail.com>
2020-12-15 15:42:13 +0000

    traditional despatch

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,31 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === 'function' ? action(state) : action),
-})
+// const countReducer = (state, action) => ({
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count: state.count + action.step}
+    case 'DECREMENT':
+      return {count: state.count - action.step}
+    case 'RESET':
+      return init(action.payload)
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`)
+  }
+}
 const init = initialCount => ({count: initialCount})
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, despatch] = React.useReducer(countReducer, initialCount, init)
+  const [state, despatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
   const {count} = state
-  const increment = () =>
-    despatch(currentState => ({count: currentState.count + step}))
+  const increment = () => despatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
